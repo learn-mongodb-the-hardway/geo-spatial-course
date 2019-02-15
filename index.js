4a8f74ec57d22bf3cd21b3b772bf45d2ece23d83
Christian Amor Kvalheim <christkv@gmail.com>
2019-02-15 16:07:07 +0100

    adding scaffolding and test cases for mapbox geo-coding

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { readFileSync } = require('fs');
 const app = express();
+const accessToken = readFileSync('./token.txt', 'utf8');
 
 app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
-  res.render('index.ejs', { acessToken: "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw" });
+  res.render('index.ejs', { acessToken: accessToken });
 });
 
 app.listen(8080);
---
+++ test/lib/mapbox_test.js
@@ -0,0 +1,27 @@
+const { MapBoxServices } = require('../../lib/mapbox');
+const { readFileSync } = require('fs');
+const assert = require('assert');
+// Check if env has been set
+var accessToken = process.env["MAPBOX_ACCESS_TOKEN"];
+
+if (accessToken == null) {
+  accessToken = readFileSync(`${__dirname}/../../token.txt`, 'utf8');
+}
+
+describe("Map Box Tests", () => {
+
+  before(async () => {});
+
+  beforeEach(async () => {});
+
+  after(async () => {});
+
+  afterEach(async () => {});
+
+  it("should correctly encode address", async () => {
+    const service = new MapBoxServices(accessToken);
+    var results = await service.geoCode("Calle Doctor Casal 16, Oviedo, Asturias");
+    console.dir(results)
+    assert.notEqual(results, null);
+  });
+});
