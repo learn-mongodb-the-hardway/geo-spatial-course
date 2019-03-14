03f8f0f23d7dea6e0ff356cf3971c90933696f51
Christian Amor Kvalheim <christkv@gmail.com>
2019-03-14 15:49:30 +0100

    refactoring admin client for simpler testing

--- public/client.js
+++ public/client.js
@@ -1,29 +1,23 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const { setDiv, postJSON, getGeoLocation } = require('./shared');
+const { setDiv, postJSON } = require('./shared');
+const { Leaflet } = require('./leaflet');
+const { GeoLocation } = require('./geo_location');
 
 var AdminClient = function(options) {
   this.mymap = null;
-  this.currentLocation = null;
-  this.currentLocationMarker = null;
-  this.intervalId = null;
   this.options = options;
-  
-  // current look up pub index
-  this.nextPubIndex = 0;
+  this.location = new GeoLocation();
 }
 
 AdminClient.prototype.setup = function() {
   var self = this;
-  var mapDivId = self.options.mapDivId;
-  var accessToken = self.options.accessToken;
 
-  // Set up some variables to track map size for
-  var bigmapheight = 600;
-  var smallmapheight = 400;
-  var mapbreakwidth = 600;
-  var highzoom = 8;
-  var lowzoom = 7;
-  var initzoom;
+  // Create a leaflet container
+  this.leaflet = new Leaflet(
+    self.options.mapDivId, self.options.accessToken);
+
+  // Initialize
+  this.leaflet.init();
 
   // Unpack options
   var optionLocation = self.options.location
@@ -31,138 +25,81 @@ AdminClient.prototype.setup = function() {
   var pubs = self.options.pubs;
   var locationDistance = self.options.locationDistance;
   
-  // Set the initial coordinates
-  var initialCoordinates = [51.503605, -0.106506];
+  // Get the current location
+  this.location.on('location', function(location) {
+    // Centering location
+    var centeringLocation = [location.latitude, location.longitude];
 
-  // Override the location
-  if (optionLocation && optionLocation.center) {
-    initialCoordinates = [optionLocation.center[1], optionLocation.center[0]];  
-  }
+    // Location is defined, draw on the map
+    if (optionLocation && optionLocation.center) {
+      // GeoJSON stores in order [longitude, latitude]
+      self.leaflet.addPointMarkerAndCircle(
+        optionLocation.center[1],
+        optionLocation.center[0], 
+        locationDistance,
+        optionLocation.placeName
+      )
 
-  // Create the mapbox view
-  self.mymap = L.map(mapDivId).setView(initialCoordinates, 1);
+      // GeoJSON stores in order [longitude, latitude]
+      centeringLocation = [optionLocation.center[1], optionLocation.center[0]];
+    }
 
-  //Set initial mapheight, based on the calculated width of the map container
-  if ($("#" + mapDivId).width() > mapbreakwidth) {
-    initzoom = highzoom;
-    $("#" + mapDivId).height(bigmapheight);
-  } else {
-    initzoom = lowzoom;
-    $("#" + mapDivId).height(smallmapheight);
-  };
+    // Center the map
+    self.leaflet.center(centeringLocation[0], centeringLocation[1], 16);
 
-  // Invalidate the size of the map
-  self.mymap.invalidateSize();
-  //Use Leaflets resize event to set new map height and zoom level
-  self.mymap.on('resize', function(e) {
-    if (e.newSize.x < mapbreakwidth) {
-      self.mymap.setZoom(lowzoom);
-      $("#" + mapDivId).css('height', smallmapheight);
-      self.mymap.invalidateSize();
-    };
+    // Map the search markers
+    // Add the markers to the leaflet
+    self.leaflet.addMarkers(searchPubs.map(function(pub) {
+      return {
+        url: self.options.url,
+        id: self.options.id,
+        name: pub.name,
+        _id: pub._id,
+        geometry: pub.geometry
+      }
+    }), function(marker) {
+      return function() {
+        addPub(marker);
+      }
+    });
+
+    // Map the crawl pubs
+    // Add the markers to the leaflet
+    self.leaflet.addMarkers(pubs.map(function(pub) {
+      return {
+        name: pub.name,
+        geometry: pub.geometry
+      }
+    }));
     
-    if (e.newSize.x > mapbreakwidth) {
-      self.mymap.setZoom(highzoom);
-      $("#" + mapDivId).css('height', bigmapheight);
-      self.mymap.invalidateSize();
-    };
+    // If we have some pubs move to them
+    if (searchPubs.length) {
+      self.leaflet.moveToLocation(searchPubs.map(function(pub) {
+        return pub.geometry;
+      }));
+    }
   });
 
-  // Set up the map
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + accessToken, {
-    zoom: initzoom,
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-  }).addTo(self.mymap);
-
-  // Get the current location
-  getGeoLocation(function(err, location) {
+  this.location.on('error', function(err) {
     if (err) {
       if (typeof console != 'undefined') console.log(err);
       return alert("Please Enable Location Support, and reload the page");
     }
-
-    // Save the current location
-    self.currentLocation = [location.latitude, location.longitude];
-
-    // Override the location
-    if (optionLocation && optionLocation.center) {
-      self.currentLocation = [optionLocation.center[1], optionLocation.center[0]];  
-    }
-
-    // Center the map on the location
-    self.mymap.setView(self.currentLocation, 16)
-
-    // Location is defined, draw on the map
-    if (optionLocation && optionLocation.center) {
-      // Add starting point shape
-      var layer = L.geoJSON(optionLocation.geometry);
-      layer.bindTooltip(optionLocation.placeName);
-      layer.addTo(self.mymap);
-
-      // Create circle
-      L.circle([optionLocation.center[1], optionLocation.center[0]], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: locationDistance
-      }).addTo(self.mymap);
-    }
-
-    // Render any of search result pubs
-    searchPubs.forEach(function(pub) {
-      var layer = L.geoJSON(pub.geometry)
-      layer.bindTooltip(pub.name);
-      layer.on('click', function() { 
-        addPub(self.options, pub);
-      });
-
-      layer.addTo(self.mymap);
-    });
-
-    // Render any pubs in the walk
-    pubs.forEach(function(pub, index) {
-      var markerOptions = {
-        radius: 12, fillColor: "green", color: "#000",
-        weight: 1, opacity: 1, fillOpacity: 0.8      
-      }
-
-      L.geoJSON(pub.geometry, {
-        pointToLayer: function(feature, latlng) {
-          return L.circleMarker(latlng, markerOptions);
-        },
-        onEachFeature: function(feature, layer) {
-          layer.bindTooltip("" + index + ". " + pub.name);
-        }
-      }).addTo(self.mymap);
-    });
-   
-    // If we have some pubs move to them
-    if (searchPubs.length) {
-      // Get the first element
-      if (searchPubs[0].geometry.type == "Point") {
-        var coordinates = [
-          searchPubs[0].geometry.coordinates[1], 
-          searchPubs[0].geometry.coordinates[0]];  
-      } else {
-        var coordinates = [
-          searchPubs[0].geometry.coordinates[0][0][1], 
-          searchPubs[0].geometry.coordinates[0][0][0]];
-      }
-
-      self.mymap.flyTo(
-        L.latLng(coordinates), 
-        self.mymap.getZoom());
-    }
   });
+
+  // Get current location
+  this.location.location();
+
+  // // Get the current location
+  // getGeoLocation(function(err, location) {
+  // });
 }
 
-function addPub(options, pub) {
+function addPub(marker) {
   // Post the pub id
-  postJSON(options.url + "/add/" + options.id, pub, { parseJSON: false }, function(err, result) {
+  postJSON(marker.url + "/add/" + marker.id, {
+    _id: marker._id
+  }, { parseJSON: false }, function(err, result) {
     if (err) {
       return alert("Failed to add pub to the pub crawl");
     }
@@ -173,7 +110,40 @@ function addPub(options, pub) {
 }
 
 module.exports = { AdminClient, addPub }
-},{"./shared":4}],2:[function(require,module,exports){
+},{"./geo_location":2,"./leaflet":4,"./shared":6}],2:[function(require,module,exports){
+function notifyAll(listeners, event, obj) {
+  if (listeners[event]) {
+    listeners[event].forEach(function(cb) {
+      cb(obj);
+    });
+  }
+}
+
+var GeoLocation = function(options) {  
+  this.listeners = {};
+  this.options = options || {};
+}
+
+GeoLocation.prototype.on = function(event, callback) {
+  if (!this.listeners[event]) {
+    this.listeners[event] = [];
+  }
+
+  this.listeners[event].push(callback);
+}
+
+GeoLocation.prototype.location = function() {
+  var self = this;  
+  // Attempt to get current location
+  navigator.geolocation.getCurrentPosition(function(position) {
+    notifyAll(self.listeners, 'location', position);
+  }, function(error) {
+    notifyAll(self.listeners, 'error', error);
+  }, this.options);  
+}
+
+module.exports = { GeoLocation };
+},{}],3:[function(require,module,exports){
 // Import all the classes
 const {AdminClient, addPub } = require('./admin_client');
 const { PubCrawlClient, mobileSetup } = require('./mobile_client');
@@ -183,7 +153,143 @@ window.AdminClient = AdminClient;
 window.addPub = addPub;
 window.PubCrawlClient = PubCrawlClient;
 window.mobileSetup = mobileSetup;
-},{"./admin_client":1,"./mobile_client":3}],3:[function(require,module,exports){
+},{"./admin_client":1,"./mobile_client":5}],4:[function(require,module,exports){
+var Leaflet = function(mapDivId, accessToken, options) {
+  this.mapDivId = mapDivId;
+  this.accessToken = accessToken;
+  this.options = options || {};
+}
+
+Leaflet.prototype.init = function() {
+  var self = this;
+  // Unpack options
+  var optionLocation = this.options.location
+  var bigmapheight = this.options.bigmapheight || 600;
+  var smallmapheight = this.options.smallmapheight || 400;
+  var mapbreakwidth = this.options.mapbreakwidth || 600;
+  var highzoom = this.options.highzoom || 8;
+  var lowzoom = this.options.lowzoom || 7;
+
+  // Set the initial coordinates
+  var initialCoordinates = [51.503605, -0.106506];
+
+  // Override the location
+  if (optionLocation && optionLocation.center) {
+    initialCoordinates = [optionLocation.center[1], optionLocation.center[0]];  
+  }
+  
+  // Create the mapbox view
+  this.mymap = L.map(this.mapDivId).setView(initialCoordinates, 1);
+
+  // Map div name
+  var mapDivName = "#" + this.mapDivId;
+
+  //Set initial mapheight, based on the calculated width of the map container
+  if ($(mapDivName).width() > mapbreakwidth) {
+    initzoom = highzoom;
+    $(mapDivName).height(bigmapheight);
+  } else {
+    initzoom = lowzoom;
+    $(mapDivName).height(smallmapheight);
+  };
+
+  // Invalidate the size of the map
+  this.mymap.invalidateSize();
+  //Use Leaflets resize event to set new map height and zoom level
+  this.mymap.on('resize', function(e) {
+    if (e.newSize.x < mapbreakwidth) {
+      self.mymap.setZoom(lowzoom);
+      $(mapDivName).css('height', smallmapheight);
+      self.mymap.invalidateSize();
+    };
+    
+    if (e.newSize.x > mapbreakwidth) {
+      self.mymap.setZoom(highzoom);
+      $(mapDivName).css('height', bigmapheight);
+      self.mymap.invalidateSize();
+    };
+  });
+
+  // Set up the map
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + this.accessToken, {
+    zoom: initzoom,
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(this.mymap);
+}
+
+Leaflet.prototype.center = function(latitude, longitude, zoom) {
+  zoom = zoom || this.mymap.getZoom();
+  this.mymap.setView([latitude, longitude], zoom);
+}
+
+Leaflet.prototype.addPointMarkerAndCircle = function(latitude, longitude, radius, tooltip) {
+  // Add starting point shape
+  var layer = L.geoJSON({
+    type: 'Point', coordinates: [longitude, latitude]
+  });
+
+  layer.bindTooltip(tooltip);
+  layer.addTo(this.mymap);
+
+  // Create circle
+  L.circle([latitude, longitude], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: radius
+  }).addTo(this.mymap);
+}
+
+Leaflet.prototype.addMarkers = function(markers, callback, options) {
+  var self = this;
+  if (typeof options === 'function') (callback = options), (options = {});
+  options = Object.assign({
+    radius: 12, fillColor: "green", color: "#000",
+    weight: 1, opacity: 1, fillOpacity: 0.8
+  }, options || {});
+  
+  // Render each marker
+  markers.forEach(function(marker, index) {
+    var layer = L.geoJSON(marker.geometry, {
+      pointToLayer: function(feature, latLng) {
+        return L.circleMarker(latLng, options);
+      },
+      onEachFeature: function(feature, layer) {
+        layer.bindTooltip("" + (index + 1) + ". " + marker.name);
+      }
+    }).addTo(self.mymap);
+
+    if (typeof callback == 'function') {
+      layer.on('click', callback(marker));      
+    }
+  });
+}
+
+Leaflet.prototype.moveToLocation = function(entries) {
+  if (entries.length) {
+      // Get the first element
+      if (entries[0].type == "Point") {
+        var coordinates = [
+          entries[0].coordinates[1], 
+          entries[0].coordinates[0]];  
+      } else {
+        var coordinates = [
+          entries[0].coordinates[0][0][1], 
+          entries[0].coordinates[0][0][0]];
+      }
+
+      this.mymap.flyTo(
+        L.latLng(coordinates), 
+        this.mymap.getZoom());
+  }
+}
+
+module.exports = { Leaflet };
+},{}],5:[function(require,module,exports){
 const { setDiv, getJSON, postJSON, getGeoLocation } = require('./shared');
 
 var PubCrawlClient = function(options) {
@@ -355,7 +461,7 @@ function mobileSetup(options) {
 }
 
 module.exports = { PubCrawlClient, mobileSetup }
-},{"./shared":4}],4:[function(require,module,exports){
+},{"./shared":6}],6:[function(require,module,exports){
 class Location {
   constructor(doc) {
     this.timestamp = doc.timestamp;
@@ -456,4 +562,4 @@ function setDiv(id, text) {
 }
 
 module.exports = { Location, setDiv, getJSON, postJSON, getGeoLocation }
-},{}]},{},[2]);
+},{}]},{},[3]);
---
+++ test/lib/frontend/admin_client_test.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const ejs = require('ejs');
+const { JSDOM } = require("jsdom");
+const { AdminClient, addPub } = require('../../../lib/frontend/admin_client');
+
+describe("AdminClient", () => {
+  before(async () => {});
+
+  after(async () => {});
+
+  beforeEach(async () => {});
+
+  afterEach(async () => {});
+
+  describe("/mobile/attendants get", async () => {
+
+    it('successfully execute setup', async () => {
+      // const client = new AdminClient({
+      //   mapDivId: 'map',
+      //   accessToken: 'peterparker',
+      //   location: {},
+      //   locationDistance: 100,
+      //   searchPubs: [],
+      //   pubs: [],
+      // });
+
+      // client.setup();
+    });
+
+  });
+});
